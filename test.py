12c9d7e1220522d057a44dd50b3bae57f12bd021
Shashank Shrivastatava <shashank29896@gmail.com>
2023-09-08 18:16:10 +0530

    first commit as outsider

---
+++ test.py
@@ -0,0 +1,6 @@
+print("hello world")
+# import langchain libraries to make a llm chain with openai
+dic = {1: "this is number one", 2: "this is number two", 3: "this is number 3"}
+text = "There are some text that needs to be printed here to check that whether the Black formatting is working correctly and break this big text into multiple lines"
+
+print("the value here")
